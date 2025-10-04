fb0f317a39a86c14991e21e55995f59820469a8b
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2025-10-04 14:30:26 +0530

    update: added sample routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,17 @@
-import "../global.css";
 import { Text, View } from "react-native";
+import { Link } from 'expo-router';
+import '@/styles/global.css';
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
-        Welcome to NativeWind!
+        Home screen
       </Text>
+      
+      <Link className="text-xl pt-5" href="/profile">move to profile screen</Link>
+      <Link className="text-xl pt-5" href="/about">move to about screen</Link>
+      
     </View>
   );
 }
