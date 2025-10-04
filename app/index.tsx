312b02aefe70688b337df00909fff76943c7f897
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2025-10-04 16:43:58 +0530

    update: testing routes

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,24 @@
-import { Text, View } from "react-native";
-import { Link } from 'expo-router';
 import '@/styles/global.css';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Link } from 'expo-router';
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-2xl font-bold text-black absolute top-10">
         Home screen
       </Text>
       
-      <Link className="text-xl pt-5" href="/profile">move to profile screen</Link>
-      <Link className="text-xl pt-5" href="/about">move to about screen</Link>
+      <View className='flex-col gap-2'>
+        <Link className="flex-row items-center" href="/profile">move to profile screen
+        <AntDesign className="ml-2" name="arrow-right" size={12} color="black" />
+      </Link>
+      
+      <Link className="flex-row items-center" href="/about">move to about screen
+        <AntDesign className="ml-2" name="arrow-right" size={12} color="black" />
+      </Link>
+      </View>
       
     </View>
   );
